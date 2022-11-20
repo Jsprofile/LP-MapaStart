cf2aec7d702a03644ac35f4abe6ae925c0d89e16
luca viegas <jsdeluca01@gmail.com>
2022-11-20 17:13:15 -0300

    adding Popup feature and fixing bugs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const barsContainer = document.querySelector(".bar")
 const nav = document.querySelector("nav")
 
 
-barsContainer.addEventListener('click', ()=>{
+barsContainer.addEventListener('click', () => {
     barsContainer.classList.toggle("activeBars")
     nav.classList.toggle('activeNav')
 })
@@ -14,11 +14,48 @@ barsContainer.addEventListener('click', ()=>{
 const quests = document.querySelectorAll('.quest')
 
 
-quests.forEach((quest)=>{
-    
+quests.forEach((quest) => {
+
     const answer = quest.nextElementSibling
-    
-    quest.addEventListener('click', ()=>{
+
+    quest.addEventListener('click', () => {
         answer.classList.toggle('ActiveAnswer')
     })
+})
+
+
+
+
+const mainBtn = document.querySelectorAll('.mainBtn')
+const wrapper = document.querySelector('.popupWrapper')
+const closer = document.querySelector('.closer')
+
+mainBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        wrapper.style.display = 'flex'
+    })
+})
+
+
+closer.addEventListener('click', () => {
+    wrapper.style.display = 'none'
+})
+
+
+
+
+const submitButton = document.querySelector('.sib-form-block__button')
+const sucess = document.querySelector('#success-message')
+
+submitButton.addEventListener('click', () => {
+
+    setTimeout(()=>{
+        if (sucess.classList.contains('sib-form-message-panel--active')) {
+            window.open('https://api.whatsapp.com/send?phone=5511985871507&amp;text=Ol%C3%A1,%20gostaria%20de%20adquirir%20meu%20Mapa%20Start!')
+        } else {
+            console.log('error')
+        }
+    }, 3000)
+
+
 })
